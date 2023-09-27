66a724652ba396489aee3cbdcf4af885dea656c4
Saifullah-Shuvo <shuvo70544@gmail.com>
2023-09-27 10:15:53 +0600

    Admin guard final

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,48 @@
-<h1>Admin Dashboard</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Steveblog | Dashboard</title>
+    {{-- User sidebar CSS --}}
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+    <x-app-layout>
+        <div class="container p-1 text-center bg-info text-white">
+            <h4>
+                {{ Auth::user()->name }}{{ __('\'s Dashboard') }}
+            </h4>
+        </div>
+        <div class="d-flex" id="wrapper">
+            <!-- Sidebar-->
+            <hr>
+            <hr>
+            @include('frontend.userdashboard.sidebar')
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">Simple Sidebar</h1>
+                    <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+                    <p>
+                        Make sure to keep all page content within the
+                        <code>#page-content-wrapper</code>
+                        . The top navbar is optional, and just for demonstration. Just create an element with the
+                        <code>#sidebarToggle</code>
+                        ID which will toggle the menu when clicked.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+    {{-- User Sidebar JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="usersidebar/js/scripts.js"></script>
+</body>
+</html>
